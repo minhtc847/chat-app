3ef8416f5170040d8ce13bfb8509b3778dbb297b
NEIHT1612 <cuongthienthong@gmail.com>
2024-12-11 11:28:48 +0700

    rollback

--- BE/cmd/api/main.go
+++ BE/cmd/api/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"BE-chat-app/cmd/api/db"
-	"BE-chat-app/cmd/api/routes"
+import "fmt"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
-)
-
-
-
-func main(){
-	db.Init()
-
-	server := gin.Default()
-
-	routes.RegisterRoutes(server)
-
-	server.Run(":8080")
+func main() {
+	fmt.Printf("Hello world!")
 }
